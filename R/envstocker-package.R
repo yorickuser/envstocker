de22ba050b7a21f562f6be707ae6b6c7fa353d29
yorickuser <hiroshibeetle@gmail.com>
2021-07-23 17:47:33 +0900

    slightly adjusted

--- R/envstocker-package.R
+++ R/envstocker-package.R
@@ -1,8 +1,22 @@
+#  copyright (C) 2018 Hiroshi C. Ito
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+
 #' stock and load execution environments of functions for debugging
 #' @aliases envstocker envstocker-package
 #' @keywords internal
 "_PACKAGE"
-
 
        
 ##' This function converts a list of environments to a normal list 
